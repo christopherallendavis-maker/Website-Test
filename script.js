989af88d658d1f92cb263454561c07520eb24867
christopherallendavis <christopherallendavis@gmail.com>
2026-02-14 22:08:53 -0500

    Publish corporate landing page from repo root

---
+++ script.js
@@ -0,0 +1,7 @@
+const startBtn = document.getElementById('startBtn');
+
+if (startBtn) {
+  startBtn.addEventListener('click', () => {
+    alert('Thank you. A consulting advisor will follow up shortly.');
+  });
+}
